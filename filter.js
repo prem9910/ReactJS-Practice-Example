6a39828ef005fa0e3d89bd9d8251cb81bf0be78e
Prem Prakash <iamprem021@gmail.com>
2024-02-02 14:33:44 +0530

    second commit

---
+++ filter.js
@@ -0,0 +1,5 @@
+const numbers = [10, 20, 30, 40, 50];
+
+const filteredNumbers = numbers.filter((number) => number > 30);
+
+console.log(filteredNumbers); // Output: [40, 50]
---
+++ map.js
@@ -0,0 +1,7 @@
+const originalArray = [1, 2, 3, 4, 5];
+
+const squaredArray = originalArray.map((number) => number * number);
+
+console.log(squaredArray); // Output: [1, 4, 9, 16, 25]
+
+
