2d5da696c15c39b676bebc7d02201eb893670a7f
Prem Prakash <iamprem021@gmail.com>
2024-02-02 11:18:14 +0530

    example added

---
+++ app.js
@@ -0,0 +1,7 @@
+let person = { name: "prem", age: 24 };
+let contact = { phone: 9910885729 };
+let address = { state: "Rajasthan", city: "jaipur", pin: 330251 };
+let clg = { ClgName: "NSTI", clgcity: "bengalore" };
+let details = { ...person,...contact,...address,...clg};
+
+console.log(details);
